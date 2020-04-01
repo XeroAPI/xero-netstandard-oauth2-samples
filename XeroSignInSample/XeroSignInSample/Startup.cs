b443c7cb6ede6f83be6e34f52c6b5521e740e23c
Matthew Mortimer <matthew.mortimer@xero.com>
2020-04-01 16:33:51 +1300

    Upgrade XeroSignInSample to dotnet core 3.1

--- XeroSignInSample/XeroSignInSample/Startup.cs
+++ XeroSignInSample/XeroSignInSample/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace XeroSignInSample
 {
@@ -53,11 +54,11 @@ namespace XeroSignInSample
                 options.CallbackPath = "/signin-oidc";
             });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -69,15 +70,15 @@ namespace XeroSignInSample
             }
 
             app.UseStaticFiles();
+            app.UseRouting();
             app.UseCookiePolicy();
-
             app.UseAuthentication();
+            app.UseAuthorization();
 
-            app.UseMvc(routes =>
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
